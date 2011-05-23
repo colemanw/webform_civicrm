fc4e819b68976560084ea06942e7adb40abb2b63
Coleman Watts <colemanwatts@gmail.com>
2011-05-23 14:46:49 -0700

    AHAH multi-contact form now works, minus a little AHAH.

--- webform_civicrm_forms.js
+++ webform_civicrm_forms.js
@@ -1,14 +1,6 @@
 if (Drupal.jsEnabled) {
   $(document).ready( function(){
 
-    $('#edit-nid').change( function(){
-      if( $(this).is(':checked') ){
-        $('#webform-civicrm-configure-form fieldset div').show(600);
-      }else{
-        $('#webform-civicrm-configure-form fieldset div').hide(600);
-      }
-    }).change();
-
     $('#edit-toggle-message').change( function(){
       if( $(this).is(':checked') ){
         $('#edit-message').removeAttr('disabled');
