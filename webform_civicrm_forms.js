bb25e63294648fa5ba17d4f11d1da016cabc0a3f
Coleman Watts <colemanwatts@gmail.com>
2011-05-23 19:14:18 -0700

    More form tweaks.

--- webform_civicrm_forms.js
+++ webform_civicrm_forms.js
@@ -1,13 +1,30 @@
 if (Drupal.jsEnabled) {
   $(document).ready( function(){
 
-    $('#edit-toggle-message').change( function(){
+    $('#edit-nid').change(function(){
       if( $(this).is(':checked') ){
+				$('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').show(600);
+      }else{
+				$('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').hide(600);
+      }
+    });
+
+    $('#edit-toggle-message').change(function(){
+      if( $(this).is(':checked')){
         $('#edit-message').removeAttr('disabled');
+				$('#edit-message-wrapper').show(600).removeClass('hidden');
       }else{
          $('#edit-message').attr('disabled','disabled');
+				$('#edit-message-wrapper').hide().addClass('hidden');
       }
     }).change();
-
+		
+		$('#edit-number-of-contacts').change(function(){
+			$('#webform-civicrm-configure-form')[0].submit();
+		});
+		
+		if(!$('#edit-nid').is(':checked')){
+			$('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').hide();
+    }
   });
 }
