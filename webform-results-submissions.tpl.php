b3e59311bbf68b93ba6d4e1ac3cb2a94471398df
Coleman Watts <colemanwatts@gmail.com>
2011-07-30 14:35:33 -0700

    Ran 6.x through Coder Upgrade... needs work

--- webform-results-submissions.tpl.php
+++ webform-results-submissions.tpl.php
@@ -4,19 +4,19 @@
  * @file
  * Result submissions override for Webform_CiviCRM
  * Displays real names instead of user names on CiviCRM enabled forms
- * NOTE: This template override is a temporary fix until drupal.org/node/1067486 is resolved
- * TODO: Once webform module has the necessary username theme funciton in place, this template will not be necessary
  */
 
-if (count($table['#rows']) && $node->webform_civicrm) {
+if (count($table['#rows']) && !empty($node->webform_civicrm)) {
   $access = user_access('access CiviCRM');
   foreach ($table['#rows'] as &$row) {
-    if (($cid = $submissions[$row[0]]->civicrm['contact_id']) && ($name = $submissions[$row[0]]->civicrm['display_name'])) {
-      if ($access) {
-        $row[2] = l($name, 'civicrm/contact/view', array('query' => array('cid' => $cid, 'reset' => 1)));
-      }
-      else {
-        $row[2] = $name;
+    if (!empty($submissions[$row[0]]->civicrm)) {
+      if (($cid = $submissions[$row[0]]->civicrm['contact_id']) && ($name = $submissions[$row[0]]->civicrm['display_name'])) {
+        if ($access) {
+          $row[2] = l($name, 'civicrm/contact/view', array('query' => 'reset=1&cid=' . $cid[1]));
+        }
+        else {
+          $row[2] = $name;
+        }
       }
     }
   }
@@ -25,19 +25,21 @@ if (count($table['#rows']) && $node->webform_civicrm) {
 /**
  * Standard webform template follows
  */
+
+drupal_add_css(drupal_get_path('module', 'webform') . '/css/webform-admin.css', 'theme', 'all', FALSE);
 ?>
+
 <?php if (count($table['#rows'])): ?>
-  <?php print theme('webform_results_per_page', array('total_count' => $total_count, 'pager_count' => $pager_count)); ?>
-  <?php print render($table); ?>
+  <?php print theme('webform_results_per_page', $total_count, $pager_count); ?>
+  <?php print theme('table', $table['#header'], $table['#rows']); ?>
 <?php else: ?>
   <?php print t('There are no submissions for this form. <a href="!url">View this form</a>.', array('!url' => url('node/' . $node->nid))); ?>
 <?php endif; ?>
 
-
 <?php if ($is_submissions): ?>
-  <?php print theme('links', array('links' => array('webform' => array('title' => t('Go back to the form'), 'href' => 'node/' . $node->nid)))); ?>
+  <?php print theme('links', array('webform' => array('title' => t('Go back to the form'), 'href' => 'node/' . $node->nid))); ?>
 <?php endif; ?>
 
 <?php if ($pager_count): ?>
-  <?php print theme('pager', array('limit' => $pager_count)); ?>
+  <?php print theme('pager', NULL, $pager_count, 0); ?>
 <?php endif; ?>
---
+++ webform_civicrm_options.js
@@ -0,0 +1,13 @@
+$(document).ready( function(){
+  $('input.civicrm-enabled').change(function(){
+    if( $(this).is(':checked') ){
+      $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').removeAttr('disabled');
+    }else{
+      $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').attr('disabled', 'disabled');
+    }
+    if ($(this).parents('tr').find('input.civicrm-label').val() == '') {
+      var val = $(this).parents('tr').find('span.civicrm-option-name').text();
+      $(this).parents('tr').find('input.civicrm-label').val(val);
+    }
+  }).change();
+});
