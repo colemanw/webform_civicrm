d2558e3a688baa305dc36a7ad6b46c8af7e338a7
Coleman Watts <colemanwatts@gmail.com>
2011-08-05 13:32:35 -0700

    Fix a number of D7 compatibility issues

--- webform_civicrm_forms.js
+++ webform_civicrm_forms.js
@@ -45,30 +45,32 @@ function webform_civicrm_relationship_options() {
     });
   }
 }
-$(document).ready( function(){
-  $('#edit-nid').change(function(){
-    if( $(this).is(':checked') ){
-      $('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').show(600);
-    }else{
-      $('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').hide(600);
+(function ($) {
+  $(document).ready( function(){
+    $('#edit-nid').change(function(){
+      if( $(this).is(':checked') ){
+        $('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').show(600);
+      }else{
+        $('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').hide(600);
+      }
+    });
+
+    $('#edit-toggle-message').change(function(){
+      if( $(this).is(':checked')){
+        $('#edit-message').removeAttr('disabled');
+        $('#edit-message-wrapper').show(600).removeClass('hidden');
+      }else{
+        $('#edit-message').attr('disabled','disabled');
+        $('#edit-message-wrapper').hide().addClass('hidden');
+      }
+    }).change();
+
+    $('#edit-number-of-contacts').change(function(){
+      $('#webform-civicrm-configure-form')[0].submit();
+    });
+
+    if(!$('#edit-nid').is(':checked')){
+      $('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').hide();
     }
   });
-
-  $('#edit-toggle-message').change(function(){
-    if( $(this).is(':checked')){
-      $('#edit-message').removeAttr('disabled');
-      $('#edit-message-wrapper').show(600).removeClass('hidden');
-    }else{
-      $('#edit-message').attr('disabled','disabled');
-      $('#edit-message-wrapper').hide().addClass('hidden');
-    }
-  }).change();
-
-  $('#edit-number-of-contacts').change(function(){
-    $('#webform-civicrm-configure-form')[0].submit();
-  });
-
-  if(!$('#edit-nid').is(':checked')){
-    $('#webform-civicrm-configure-form fieldset > div, #webform-civicrm-configure-form fieldset > fieldset, #edit-number-of-contacts-wrapper').not('.hidden').hide();
-  }
-});
+})(jQuery);
--- webform_civicrm_options.js
+++ webform_civicrm_options.js
@@ -1,13 +1,15 @@
-$(document).ready( function(){
-  $('input.civicrm-enabled').change(function(){
-    if( $(this).is(':checked') ){
-      $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').removeAttr('disabled');
-    }else{
-      $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').attr('disabled', 'disabled');
-    }
-    if ($(this).parents('tr').find('input.civicrm-label').val() == '') {
-      var val = $(this).parents('tr').find('span.civicrm-option-name').text();
-      $(this).parents('tr').find('input.civicrm-label').val(val);
-    }
-  }).change();
-});
+(function ($) {
+  $(document).ready( function(){
+    $('input.civicrm-enabled').change(function(){
+      if( $(this).is(':checked') ){
+        $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').removeAttr('disabled');
+      }else{
+        $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').attr('disabled', 'disabled');
+      }
+      if ($(this).parents('tr').find('input.civicrm-label').val() == '') {
+        var val = $(this).parents('tr').find('span.civicrm-option-name').text();
+        $(this).parents('tr').find('input.civicrm-label').val(val);
+      }
+    }).change();
+  });
+})(jQuery);
