8b7bb8d603c5ebf25c0c41771771809a30d1b5b9
Coleman Watts <colemanwatts@gmail.com>
2011-08-05 21:58:51 -0700

    A few more D7 compatibility issues

--- webform_civicrm_options.js
+++ webform_civicrm_options.js
@@ -4,7 +4,7 @@
       if( $(this).is(':checked') ){
         $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').removeAttr('disabled');
       }else{
-        $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').attr('disabled', 'disabled');
+        $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').attr('disabled', 'disabled').removeAttr('checked');
       }
       if ($(this).parents('tr').find('input.civicrm-label').val() == '') {
         var val = $(this).parents('tr').find('span.civicrm-option-name').text();
