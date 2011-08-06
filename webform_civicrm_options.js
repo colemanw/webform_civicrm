ab6ac2bc6aca4281d89190a6c8bfa9bb910dfd3b
Coleman Watts <colemanwatts@gmail.com>
2011-08-05 22:08:30 -0700

    options.js tweak

--- webform_civicrm_options.js
+++ webform_civicrm_options.js
@@ -3,7 +3,7 @@ $(document).ready( function(){
     if( $(this).is(':checked') ){
       $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').removeAttr('disabled');
     }else{
-      $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').attr('disabled', 'disabled');
+      $(this).parents('tr').find('input.civicrm-label, input.civicrm-default').attr('disabled', 'disabled').removeAttr('checked');
     }
     if ($(this).parents('tr').find('input.civicrm-label').val() == '') {
       var val = $(this).parents('tr').find('span.civicrm-option-name').text();
