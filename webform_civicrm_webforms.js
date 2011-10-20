f4feadc773f48a42023ad478fdd1c251a6288113
colemanw <colemanwatts@gmail.com>
2011-10-19 19:47:08 -0700

    Wrap form prefix in a div, issue #1314254.

--- webform_civicrm_webforms.js
+++ webform_civicrm_webforms.js
@@ -9,14 +9,15 @@ function webform_civicrm_populate_states(stateSelect, countryId) {
   var value = $(stateSelect).val();
   $(stateSelect).attr('disabled', 'disabled');
   $.ajax({
-    url:'/webform-civicrm/js/state_province/'+countryId,
+    url: '/webform-civicrm/js/state_province/'+countryId,
+    dataType: 'json',
     success: function(data) {
       $(stateSelect).find('option').remove();
       for (key in data) {
         $(stateSelect).append('<option value="'+key+'">'+data[key]+'</option>');
       }
-      $(stateSelect).removeAttr('disabled');
       $(stateSelect).val(value);
+      $(stateSelect).removeAttr('disabled');
     }
   });
 }
@@ -27,7 +28,7 @@ $(document).ready(function(){
     var name = $(this).attr('name');
     var key = webform_civicrm_parse_name(name);
     var value = $(this).val();
-    var countrySelect = $(this).parents('form').first().find('select[name*="'+(key.replace('state_province','country' ))+'"]');
+    var countrySelect = $(this).parents('form.webform-client-form').find('select[name*="'+(key.replace('state_province','country' ))+'"]');
     var classes = $(this).attr('class').replace('text', 'select');
     $(this).replaceWith('<select id="'+id+'" name="'+name+'" class="'+classes+'"><option selected="selected" value="'+value+'"> </option></select>');
     if (countrySelect.length == 0) {
@@ -41,10 +42,11 @@ $(document).ready(function(){
   $('form.webform-client-form select[name*="_address_country_id"][name*="civicrm_"]').change(function(){
     var name = webform_civicrm_parse_name($(this).attr('name'));
     var countryId = $(this).val();
-    var stateSelect = $(this).parents('form').first().find('select[name*="'+(name.replace('country', 'state_province'))+'"]');
+    var stateSelect = $(this).parents('form.webform-client-form').find('select[name*="'+(name.replace('country', 'state_province'))+'"]');
     $(stateSelect).val('');
     webform_civicrm_populate_states(stateSelect, countryId);
   });
 });
+
 
 })(jQuery);
