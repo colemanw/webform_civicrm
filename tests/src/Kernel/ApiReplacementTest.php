f47d845bcbf91eed680bf08c59b4f1bdaa43cc27
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2019-06-19 14:42:38 +0200

    Added test for wf_crm_matching_rules

--- tests/src/Kernel/ApiReplacementTest.php
+++ tests/src/Kernel/ApiReplacementTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\webform_civicrm\Kernel;
 
+use CRM_Core_DAO;
+
 class ApiReplacementTest  extends CiviCRMTestBase {
 
   protected function setUp() {
@@ -22,5 +24,32 @@ class ApiReplacementTest  extends CiviCRMTestBase {
     $this->assertNotNull($glId,'Gelderland should have a code');
     $glCode =  wf_crm_state_abbr($glId,'abbreviation',$nlId);
     $this->assertEquals('GL',$glCode);
+  }
+
+  function old_wf_crm_get_matching_rules($contact_type) {
+    static $rules;
+    $contact_type = ucfirst($contact_type);
+    if (!$rules) {
+      $rules = array_fill_keys(array('Individual', 'Organization', 'Household'), array());
+      $dao = CRM_Core_DAO::executeQuery('SELECT * FROM civicrm_dedupe_rule_group');
+      while ($dao->fetch()) {
+        $rules[$dao->contact_type][$dao->id] = $dao->title;
+      }
+    }
+    return $rules[$contact_type];
+  }
+
+  public function test_CrmGetMatchingRules(){
+    
+    $types = ['Individual', 'Organization', 'Household'];
+    foreach($types as $type) {
+      $oldRules = $this->old_wf_crm_get_matching_rules($type);
+      $newRules = wf_crm_get_matching_rules($type);
+      $this->assertSame($oldRules, $newRules);
+    }
+    
+   
+
+
   }
 }
