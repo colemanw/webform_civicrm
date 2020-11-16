31e2a333d52cf2fccd2242dfd674b61148612d2e
Matt Glaman <nmd.matt@gmail.com>
2020-11-16 00:06:25 -0600

    Configure contact wizard page

--- tests/src/FunctionalJavascript/ContributionPageTest.php
+++ tests/src/FunctionalJavascript/ContributionPageTest.php
@@ -11,6 +11,15 @@ use Behat\Mink\Element\NodeElement;
  */
 final class ContributionPageTest extends WebformCivicrmTestBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function initFrontPage() {
+    parent::initFrontPage();
+    // Fix hidden columns on build page.
+    $this->getSession()->resizeWindow(1440, 900);
+  }
+
   private function createPaymentProcessor() {
     $params = [
       'domain_id' => 1,
@@ -61,15 +70,44 @@ final class ContributionPageTest extends WebformCivicrmTestBase {
     $this->getSession()->getPage()->pressButton('Save Settings');
     $this->assertSession()->pageTextContains('Saved CiviCRM settings');
     $this->getSession()->getPage()->clickLink('Build');
+    $this->getSession()->getPage()->clickLink('Add page');
+    $this->assertSession()->assertWaitOnAjaxRequest();
     $this->htmlOutput();
+    $element_form = $this->getSession()->getPage()->findById('webform-ui-element-form-ajax');
+    $element_form->fillField('Title', 'Contact information');
+    // @todo Regular tricks waiting for the machine name fail, here.
+    $element_form->pressButton('Save');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->assertSession()->pageTextContains('Key field is required.');
+    $this->getSession()->getPage()->fillField('Key', 'contact_information');
+    $element_form->pressButton('Save');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+
+    // Put contact elements into new page.
+    $contact_information_page_row_handle = $this->assertSession()->elementExists('css', '[data-drupal-selector="edit-webform-ui-elements-contact-information"] a.tabledrag-handle');
+    $contact_information_page_row_handle->keyDown(38);
+    $contact_information_page_row_handle->keyUp(38);
+    $contact_information_page_row_handle->keyDown(38);
+    $contact_information_page_row_handle->keyUp(38);
+    $contact_information_page_row_handle->blur();
+    $contact_fieldset_row_handle = $this->assertSession()->elementExists('css', '[data-drupal-selector="edit-webform-ui-elements-civicrm-1-contact-1-fieldset-fieldset"] a.tabledrag-handle');
+    $contact_fieldset_row_handle->keyDown(39);
+    $contact_fieldset_row_handle->keyUp(39);
+    $contact_fieldset_row_handle->blur();
+    $this->getSession()->getPage()->pressButton('Save elements');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+
     $this->drupalGet($this->webform->toUrl('canonical'));
-    // @todo this is failing on `Undefined index: civicrm_1_contribution_1_contribution_total_amount`
-    // This is due to contribution being in a "Wizard page" and Contacts is not.
-    // This is a manual setup that must be done to create a wizard page and move contacts into there.
     $error_messages = $this->getSession()->getPage()->findAll('css', '.messages.messages--error');
     $this->assertCount(0, $error_messages, implode(', ', array_map(static function(NodeElement $el) {
       return $el->getValue();
     }, $error_messages)));
+    $this->htmlOutput();
+    $this->getSession()->getPage()->fillField('First Name', 'Frederick');
+    $this->getSession()->getPage()->fillField('Last Name', 'Pabst');
+    $this->getSession()->getPage()->fillField('Email', 'fred@example.com');
+    $this->getSession()->getPage()->pressButton('Next >');
+    // @todo submit contribution form.
     $this->htmlOutput();
   }
 
