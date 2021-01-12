a4bf5d9a54d8a90ad5b1712fc14487b97387362d
KarinG <karin@semper-it.com>
2021-01-12 13:51:21 -0700

    Take another screenshot.

--- tests/src/FunctionalJavascript/ContributionPageTest.php
+++ tests/src/FunctionalJavascript/ContributionPageTest.php
@@ -151,6 +151,7 @@ final class ContributionPageTest extends WebformCivicrmTestBase {
 
     $this->getSession()->getPage()->fillField('Postal Code', '53177');
     $this->getSession()->getPage()->pressButton('Submit');
+    $this->htmlOutput();
     $this->assertPageNoErrorMessages();
     $this->assertSession()->pageTextContains('New submission added to CiviCRM Webform Test.');
 
