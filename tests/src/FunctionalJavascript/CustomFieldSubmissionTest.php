63fc60ce8b2e5b655ab27d6bc1e592e30162f30f
KarinG <karin@semper-it.com>
2021-01-16 10:56:06 -0700

    Block in test for Custom Fields of type: Checkboxes.

--- tests/src/FunctionalJavascript/CustomFieldSubmissionTest.php
+++ tests/src/FunctionalJavascript/CustomFieldSubmissionTest.php
@@ -43,9 +43,54 @@ final class CustomFieldSubmissionTest extends WebformCivicrmTestBase {
       'time_format' => 2,
       'is_active' => 1,
     ]);
-    // throw new \Exception(var_export($result, TRUE));
     $this->assertEquals(0, $result['is_error']);
     $this->assertEquals(1, $result['count']);
+
+    $result = civicrm_api3('OptionGroup', 'create', [
+      'name' => "checkbox_1",
+      'label' => "Checkbox",
+      'data_type' => "String",
+      'is_active' => 1,
+    ]);
+    $this->assertEquals(0, $result['is_error']);
+    $this->assertEquals(1, $result['count']);
+    $optiongroup_id = $result['id'];
+
+    $result = civicrm_api3('OptionValue', 'create', [
+      'option_group_id' => $optiongroup_id,
+      'name' => "Red",
+      'label' => "Red",
+      'value' => 1,
+      'weight' => 1,
+      'is_active' => 1,
+    ]);
+    $this->assertEquals(0, $result['is_error']);
+    $this->assertEquals(1, $result['count']);
+
+    $result = civicrm_api3('OptionValue', 'create', [
+      'option_group_id' => $optiongroup_id,
+      'name' => "Green",
+      'label' => "Green",
+      'value' => 2,
+      'weight' => 2,
+      'is_active' => 1,
+    ]);
+    $this->assertEquals(0, $result['is_error']);
+    $this->assertEquals(1, $result['count']);
+
+    $result = civicrm_api3('CustomField', 'create', [
+      'custom_group_id' => "Custom",
+      'label' => "Checkboxes",
+      'data_type' => "String",
+      'html_type' => "Checkbox",
+      'option_group_id' => $optiongroup_id,
+      'serialize' => 1,
+      'is_active' => 1,
+    ]);
+    throw new \Exception(var_export($result, TRUE));
+    $this->assertEquals(0, $result['is_error']);
+    $this->assertEquals(1, $result['count']);
+
   }
 
   /**
@@ -71,13 +116,25 @@ final class CustomFieldSubmissionTest extends WebformCivicrmTestBase {
     $this->getSession()->getPage()->checkField("civicrm_1_contact_1_cg1_custom_1");
     $this->getSession()->getPage()->checkField("civicrm_1_contact_1_cg1_custom_2");
     $this->getSession()->getPage()->checkField("civicrm_1_contact_1_cg1_custom_2_timepart");
+    $this->getSession()->getPage()->checkField("civicrm_1_contact_1_cg1_custom_3");
 
     $this->assertSession()->checkboxChecked("civicrm_1_contact_1_cg1_custom_1");
     $this->assertSession()->checkboxChecked("civicrm_1_contact_1_cg1_custom_2");
     $this->assertSession()->checkboxChecked("civicrm_1_contact_1_cg1_custom_2_timepart");
+    $this->assertSession()->checkboxChecked("civicrm_1_contact_1_cg1_custom_3");
 
     $this->getSession()->getPage()->pressButton('Save Settings');
     $this->assertSession()->pageTextContains('Saved CiviCRM settings');
+
+    // Change the Checkbox -> no Listbox (that should probably be the default)
+    $this->drupalGet($this->webform->toUrl('edit-form'));
+    $this->getSession()->getPage()->clickLink('edit-webform-ui-elements-civicrm-1-contact-1-cg1-custom-3-operations');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $element_form = $this->getSession()->getPage()->findById('properties[extra][aslist]');
+    $element_form->uncheckField('Listbox');
+    $this->assertSession()->waitForElementVisible('css', '.machine-name-value');
+    $element_form->pressButton('Save');
+    $this->assertSession()->assertWaitOnAjaxRequest();
 
     $this->drupalLogout();
     $this->drupalGet($this->webform->toUrl('canonical'));
@@ -96,6 +153,9 @@ final class CustomFieldSubmissionTest extends WebformCivicrmTestBase {
     $driver->executeScript("document.getElementById('edit-civicrm-1-contact-1-cg1-custom-2').setAttribute('value', '2020-12-12')");
     $driver->executeScript("document.getElementById('edit-civicrm-1-contact-1-cg1-custom-2-timepart').setAttribute('value', '10:20:00')");
 
+    $this->getSession()->getPage()->checkField('Red');
+    $this->getSession()->getPage()->checkField('Green');
+
     $this->getSession()->getPage()->pressButton('Submit');
     $this->assertSession()->pageTextContains('New submission added to CiviCRM Webform Test.');
 
@@ -105,8 +165,8 @@ final class CustomFieldSubmissionTest extends WebformCivicrmTestBase {
       'sequential' => 1,
       'entity_id' => 3,
     ]);
-    $this->assertEquals(2, $api_result['count']);
-    // throw new \Exception(var_export($api_result, TRUE));
+    $this->assertEquals(3, $api_result['count']);
+    throw new \Exception(var_export($api_result, TRUE));
     $this->assertEquals('Lorem Ipsum', $api_result['values'][0]['latest']);
     $this->assertEquals('2020-12-12 10:20:00', $api_result['values'][1]['latest']);
   }
