f672dac673cbe208aca27f9bcdd8f4e76b89e66c
KarinG <karin@semper-it.com>
2021-02-01 13:34:35 -0700

    Checking one Tag.

--- tests/src/FunctionalJavascript/GroupsTagsSubmissionTest.php
+++ tests/src/FunctionalJavascript/GroupsTagsSubmissionTest.php
@@ -5,7 +5,7 @@ namespace Drupal\Tests\webform_civicrm\FunctionalJavascript;
 use Drupal\Core\Url;
 
 /**
- * Tests submitting a Webform with CiviCRM: existing contact element.
+ * Tests submitting a Webform with CiviCRM: Tags
  *
  * @group webform_civicrm
  */
@@ -62,6 +62,8 @@ final class GroupsTagsSubmissionTest extends WebformCivicrmTestBase {
     // The test here is to check if the fields on the form populate with Contact details belonging to the logged in User:
     $this->assertSession()->fieldValueEquals('First Name', 'Maarten');
     $this->assertSession()->fieldValueEquals('Last Name', 'van der Weijden');
+
+    $this->getSession()->getPage()->checkField('Volunteer');
 
     $this->getSession()->getPage()->pressButton('Submit');
     $this->assertPageNoErrorMessages();
