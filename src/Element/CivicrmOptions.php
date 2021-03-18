4d0331a730fb50402c763857119581acbe70a8da
KarinG <karin@semper-it.com>
2021-03-18 16:42:21 -0600

    Removing unnecessary indentation.

--- src/Element/CivicrmOptions.php
+++ src/Element/CivicrmOptions.php
@@ -1,12 +1,12 @@
 <?php
 
-  namespace Drupal\webform_civicrm\Element;
+namespace Drupal\webform_civicrm\Element;
 
-  use Drupal\Core\Render\Element\Select;
+use Drupal\Core\Render\Element\Select;
 
-  /**
-   * @FormElement("civicrm_options")
-   */
-  class CivicrmOptions extends Select {
+/**
+ * @FormElement("civicrm_options")
+ */
+class CivicrmOptions extends Select {
 
-  }
+}
