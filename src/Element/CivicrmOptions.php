e0c42d37bb5a63c0dac24cbf44d74438719c183d
Karin Gerritsen <karin@semper-it.com>
2021-03-19 08:17:49 -0600

    Add Element CivicrmOptions - spaceholder- b/c Webform seems to be expecting it (#512)
    * Add Element CivicrmOptions - spaceholder - but Webform seems to be expecting it

---
+++ src/Element/CivicrmOptions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\webform_civicrm\Element;
+
+use Drupal\Core\Render\Element\Select;
+
+/**
+ * @FormElement("civicrm_options")
+ */
+class CivicrmOptions extends Select {
+
+}
