15e07e82ffccdbcc6db0e2327ec6e6ca4d5f2624
KarinG <karin@semper-it.com>
2021-04-02 13:01:18 -0600

    Webform is expecting an Element

---
+++ src/Element/CivicrmContact.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\webform_civicrm\Element;
+
+use Drupal\webform\Plugin\WebformElementBase;
+
+/**
+ * @FormElement("civicrm_contact")
+ */
+class CivicrmContact extends WebformElementBase {
+
+}
