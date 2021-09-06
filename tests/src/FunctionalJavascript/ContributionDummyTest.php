0c043057979b8c2ef6aa1c25313d64891476c8f2
KarinG <karin@semper-it.com>
2021-09-06 09:55:15 -0600

    Where is my screenshot.

--- tests/src/FunctionalJavascript/ContributionDummyTest.php
+++ tests/src/FunctionalJavascript/ContributionDummyTest.php
@@ -178,8 +178,9 @@ final class ContributionDummyTest extends WebformCivicrmTestBase {
     // Amounts = 10 + 20.00 + 29.50 + 100.00 + 200.00 = 359.5
     // Taxes = 1.48 + 5 + 10 = 16.48
     // Total = 359.5 + 16.48 = 375.98
+
     $this->assertSession()->elementTextContains('css', '#wf-crm-billing-total', '375.98');
-    $this->createScreenshot($this->htmlOutputDirectory . '/line_items.png');
+    $this->createScreenshot($this->htmlOutputDirectory . '/lineitems.png');
 
     $this->fillCardAndSubmit();
 
