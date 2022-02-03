f31a4b853637ee69c498a5272ef0aa8f62cf144f
KarinG <karin@semper-it.com>
2022-02-03 16:33:37 -0700

    Drafting some utils edits

--- src/Utils.php
+++ src/Utils.php
@@ -698,9 +698,9 @@ class Utils implements UtilsInterface {
     try {
       // Use the Payment Processor to attempt to take the actual payment. You may
       // pass in other params here, too.
-      $propertyBag = new \Civi\Payment\PropertyBag();
-      $propertyBag->mergeLegacyInputParams($params);
-      $propertyBag->setContributionID($order['id']);
+      ////$propertyBag = new \Civi\Payment\PropertyBag();
+      ////$propertyBag->mergeLegacyInputParams($params);
+      ////$propertyBag->setContributionID($order['id']);
       // @fixme: Class \Civi\Payment\CRM_Utils_Money not found (fixed in 5.28 via https://github.com/civicrm/civicrm-core/pull/17505
       //   But note we still return a localized format
       // $propertyBag->setAmount($params['total_amount']);
@@ -708,20 +708,27 @@ class Utils implements UtilsInterface {
       // We need the payment params as an array to pass to PaymentProcessor.Pay API3.
       // If https://github.com/civicrm/civicrm-core/pull/17507 was merged we wouldn't need this hack
       //   using ReflectionClass to access the array.
-      $reflectionClass = new \ReflectionClass($propertyBag);
-      $reflectionProperty = $reflectionClass->getProperty('props');
-      $reflectionProperty->setAccessible(TRUE);
-      $payParams = $reflectionProperty->getValue($propertyBag)['default'];
+      ////$reflectionClass = new \ReflectionClass($propertyBag);
+      ////$reflectionProperty = $reflectionClass->getProperty('props');
+      ////$reflectionProperty->setAccessible(TRUE);
+      ////$payParams = $reflectionProperty->getValue($propertyBag)['default'];
 
       // propertyBag has 'contributionID', we need 'contribution_id'.
-      $payParams['contribution_id'] = $propertyBag->getContributionID();
+      ////$payParams['contribution_id'] = $propertyBag->getContributionID();
       // propertyBag has 'currency', iATS uses `currencyID` until https://github.com/iATSPayments/com.iatspayments.civicrm/pull/350 is merged
-      $payParams['currencyID'] = $propertyBag->getCurrency();
-      $payParams['amount'] = $params['total_amount'];
+      ////$payParams['currencyID'] = $propertyBag->getCurrency();
+      $params['amount'] = $params['total_amount'];
       // For legacy purposes (if payment processor does not use propertyBag)
       if (isset($payParams['isRecur'])) {
         $payParams['is_recur'] = $payParams['isRecur'];
       }
+      $params['contribution_id'] = $order['id'];
+      ////throw new \Exception(var_export($params, TRUE));
+      $payParams = $params;
+
+      $params['payment_processor_id'] = $params['payment_processor'];
+      $params['contribution_id'] = $order['id'];
+      $params['currencyID'] = $params['currency'];
       $payResult = reset(civicrm_api3('PaymentProcessor', 'pay', $payParams)['values']);
 
       // webform_civicrm sends out receipts using Contribution.send_confirmation API if the contribution page is has is_email_receipt = TRUE.
