cd40482267588a0ec259db602d2a09c3009bb751
KarinG <karin@semper-it.com>
2022-08-08 18:03:21 -0600

    Revert "D8/9 - Fix source field to display on the contact widget using v4 api"

--- src/ContactComponent.php
+++ src/ContactComponent.php
@@ -93,46 +93,23 @@ class ContactComponent implements ContactComponentInterface {
     $limit = $str ? 12 : 500;
     $ret = [];
     $display_fields = array_values($element['#results_display']);
-    $fieldMappings = [
-      'current_employer' => ['employer_id', 'display_name'],
-      'email' => ['email', 'email'],
-      'phone' => ['phone', 'phone'],
-      'city' => ['address', 'city'],
-      'state_province' => ['address', 'state_province_id:label'],
-      'country' => ['address', 'country_id:label'],
-      'county' => ['address', 'county_id:label'],
-      'postal_code' => ['address', 'postal_code'],
-    ];
-    $joinedTables = [];
-    foreach ($fieldMappings as $field => $type) {
-      if ($key = array_search($field, $display_fields)) {
-        unset($display_fields[$key]);
-        [$table, $fieldName]= $type;
-        $display_fields[] = "{$table}.{$fieldName}";
-        if (empty($joinedTables[$table]) && in_array($table, ['email', 'phone', 'address'])) {
-          $joinedTables[$table] = TRUE;
-          $params['join'][] = [ucfirst($table) . " AS {$table}", 'LEFT', ["{$table}.is_primary", '=', 1]];
-        }
-      }
-    }
+    $search_field = 'display_name';
     $sort_field = 'sort_name';
     // Search and sort based on the selected display field
     if (!in_array('display_name', $display_fields)) {
-      $sort_field = $display_fields[0];
+      $search_field = $sort_field = $display_fields[0];
     }
     $params += [
-      'limit' => $limit,
-      'orderBy' => [
-        $sort_field => 'ASC',
-      ],
-      'select' => $display_fields,
+      'rowCount' => $limit,
+      'sort' => $sort_field,
+      'return' => $display_fields,
     ];
     if (!empty($params['relationship']['contact'])) {
       $c = $params['relationship']['contact'];
       $relations = NULL;
       if (!empty($contacts[$c]['id'])) {
         $relations = $this->wf_crm_find_relations($contacts[$c]['id'], wf_crm_aval($params['relationship'], 'types'));
-        $params['where'][] = ['id', 'IN', $relations];
+        $params['id'] = ['IN' => $relations];
       }
       if (!$relations) {
         return $ret;
@@ -140,16 +117,20 @@ class ContactComponent implements ContactComponentInterface {
     }
     unset($params['relationship']);
     if ($str) {
-      $searchFields = [];
-      foreach ($display_fields as $fld) {
-        $searchFields[] = [$fld, 'CONTAINS', $str];
+      $str = str_replace(' ', '%', \CRM_Utils_Type::escape($str, 'String'));
+      // The contact api takes a quirky format for display_name and sort_name
+      if (in_array($search_field, ['sort_name', 'display_name'])) {
+        $params[$search_field] = $str;
       }
-      $params['where'][] = ['OR', $searchFields];
+      // Others use the standard convention
+      else {
+        $params[$search_field] = ['LIKE' => "%$str%"];
+      }
     }
-    $result = $this->utils->wf_civicrm_api4('Contact', 'get', $params);
+    $result = $this->utils->wf_civicrm_api('contact', 'get', $params);
     // Autocomplete results
     if ($str) {
-      foreach ($result as $contact) {
+      foreach (wf_crm_aval($result, 'values', []) as $contact) {
         if ($name = $this->wf_crm_format_contact($contact, $display_fields)) {
           $ret[] = ['id' => $contact['id'], 'name' => $name];
         }
@@ -164,14 +145,14 @@ class ContactComponent implements ContactComponentInterface {
       if (!empty($element['#allow_create'])) {
         $ret['-'] = Xss::filter($element['#none_prompt']);
       }
-      foreach ($result as $contact) {
+      foreach (wf_crm_aval($result, 'values', []) as $contact) {
         // Select lists will be escaped by FAPI
         if ($name = $this->wf_crm_format_contact($contact, $display_fields, FALSE)) {
           $ret[$contact['id']] = $name;
         }
       }
       // If we get exactly $limit results, there are probably more - warn that the list is truncated
-      if (wf_crm_aval($result, 'rowCount') >= $limit) {
+      if (wf_crm_aval($result, 'count') >= $limit) {
         \Drupal::logger('webform_civicrm')->warning(
           'Maximum contacts exceeded, list truncated on the webform "@title". The webform_civicrm "@field" field cannot display more than @limit contacts because it is a select list. Recommend switching to autocomplete widget in element settings.',
           ['@limit' => $limit, '@field' => $element['#title'], '@title' => $node->label()]);
@@ -208,29 +189,29 @@ class ContactComponent implements ContactComponentInterface {
     if (!is_numeric($cid)) {
       return FALSE;
     }
-    $filters['where'][] = ['id', '=', $cid];
-    $filters['where'][] = ['is_deleted', '=', 0];
+    $filters['id'] = $cid;
+    $filters['is_deleted'] = 0;
     // A contact always has permission to view self
     if ($cid == $this->utils->wf_crm_user_cid()) {
-      $filters['checkPermissions'] = FALSE;
+      $filters['check_permissions'] = FALSE;
     }
-    if (!empty($filters['checkPermissions'])) {
+    if (!empty($filters['check_permissions'])) {
       // If we have a valid checksum for this contact, bypass other permission checks
       // For legacy reasons we support "cid" param as an alias of "cid1"
       // ToDo use: \Drupal::request()->query->all();
       if (wf_crm_aval($_GET, "cid$c") == $cid || ($c == 1 && wf_crm_aval($_GET, "cid") == $cid)) {
         // For legacy reasons we support "cs" param as an alias of "cs1"
         if (!empty($_GET['cs']) && $c == 1 && \CRM_Contact_BAO_Contact_Utils::validChecksum($cid, $_GET['cs'])) {
-          $filters['checkPermissions'] = FALSE;
+          $filters['check_permissions'] = FALSE;
         }
         elseif (!empty($_GET["cs$c"]) && \CRM_Contact_BAO_Contact_Utils::validChecksum($cid, $_GET["cs$c"])) {
-          $filters['checkPermissions'] = FALSE;
+          $filters['check_permissions'] = FALSE;
         }
       }
     }
     // Fetch contact name with filters applied
-    $result = $this->utils->wf_civicrm_api4('Contact', 'get', $filters)[0] ?? [];
-    return $this->wf_crm_format_contact($result, /*$component['#extra']['results_display']*/ ['display_name']);
+    $result = $this->utils->wf_civicrm_api('contact', 'get', $filters);
+    return $this->wf_crm_format_contact(wf_crm_aval($result, "values:$cid"), /*$component['#extra']['results_display']*/ ['display_name']);
   }
 
   /**
@@ -336,14 +317,13 @@ class ContactComponent implements ContactComponentInterface {
     /** @var \Drupal\webform\Plugin\WebformElementManagerInterface $element_manager */
     $element_manager = \Drupal::service('plugin.manager.webform.element');
     $contact_element = $element_manager->getElementInstance($component);
-    $params = [];
-    $params['where'][] = ['is_deleted', '=', 0];
+    $params = ['is_deleted' => 0];
     $contactFilters = [
       'contact_type',
       'contact_sub_type',
       'tag',
       'group',
-      'checkPermissions',
+      'check_permissions',
       'relationship' => [
         'contact',
         'types',
@@ -360,15 +340,7 @@ class ContactComponent implements ContactComponentInterface {
         }
       }
       else {
-        $filterVal = $contact_element->getElementProperty($component, $filter);
-        if ($filterVal) {
-          $op = '=';
-          if (in_array($filter, ['group', 'tag'])) {
-            $filter .= 's';
-            $op = 'IN';
-          }
-          $params['where'][] = [$filter, $op, $filterVal];
-        }
+        $params[$filter] = $contact_element->getElementProperty($component, $filter);
       }
     }
     return $params;
