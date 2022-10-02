1dfcade954cd99b40d31b899737f3ff73d019d69
jitendrapurohit <jitendra@fuzion.co.nz>
2022-10-02 16:28:46 +0530

    Add test to verify Child tag submission

--- tests/src/FunctionalJavascript/GroupsTagsSubmissionTest.php
+++ tests/src/FunctionalJavascript/GroupsTagsSubmissionTest.php
@@ -168,4 +168,73 @@ final class GroupsTagsSubmissionTest extends WebformCivicrmTestBase {
     $this->assertFalse(in_array($this->groups['GroupB'], $contactGroups));
   }
 
+  /**
+   * Verify if child tag is listed correctly on contact element.
+   * Also, assert submission of child tag values.
+   */
+  public function testChildTagSubmission() {
+    $this->tags['tagA'] = $this->utils->wf_civicrm_api('Tag', 'create', [
+      'name' => 'Child Tag A',
+      'parent_id' => "Company",
+    ])['id'];
+    $this->tags['tagB'] = $this->utils->wf_civicrm_api('Tag', 'create', [
+      'name' => 'Child Tag B',
+      'parent_id' => "Company",
+    ])['id'];
+
+    $this->drupalLogin($this->rootUser);
+    $this->drupalGet(Url::fromRoute('entity.webform.civicrm', [
+      'webform' => $this->webform->id(),
+    ]));
+    $this->enableCivicrmOnWebform();
+
+    $this->getSession()->getPage()->selectFieldOption('contact_1_number_of_other', 'Yes');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+
+    // Ensure child tags are present in the tags dropdown.
+    $this->assertSession()->optionExists('civicrm_1_contact_1_other_tag[]', $this->tags['tagA']);
+    $this->assertSession()->optionExists('civicrm_1_contact_1_other_tag[]', $this->tags['tagB']);
+    $this->getSession()->getPage()->selectFieldOption("civicrm_1_contact_1_other_tag[]", 'create_civicrm_webform_element');
+    $this->htmlOutput();
+    $this->saveCiviCRMSettings();
+
+    $this->drupalGet($this->webform->toUrl('edit-form'));
+    $this->htmlOutput();
+
+    $this->assertSession()->waitForElementVisible('css', "[data-drupal-selector='edit-webform-ui-elements-civicrm-1-contact-1-contact-existing-operations'] a.webform-ajax-link");
+    $contactElementEdit = $this->assertSession()->elementExists('css', "[data-drupal-selector='edit-webform-ui-elements-civicrm-1-contact-1-contact-existing-operations'] a.webform-ajax-link");
+    $contactElementEdit->click();
+    $this->htmlOutput();
+    $this->assertSession()->waitForElementVisible('css', '[data-drupal-selector="edit-form"]');
+
+    $this->assertSession()->elementExists('css', '[data-drupal-selector="edit-filters"]')->click();
+
+    $this->assertSession()->optionExists('properties[tag][]', $this->tags['tagA']);
+    $this->assertSession()->optionExists('properties[tag][]', $this->tags['tagB']);
+
+    $this->drupalGet($this->webform->toUrl('canonical'));
+    $this->assertPageNoErrorMessages();
+    $this->getSession()->getPage()->fillField('First Name', 'Frederick');
+    $this->getSession()->getPage()->fillField('Last Name', 'Pabst');
+
+    $this->getSession()->getPage()->checkField('Volunteer');
+    $this->assertSession()->checkboxChecked("Volunteer");
+    $this->getSession()->getPage()->checkField("- Child Tag A");
+    $this->assertSession()->checkboxChecked("- Child Tag A");
+
+    $this->getSession()->getPage()->pressButton('Submit');
+    $this->assertPageNoErrorMessages();
+    $this->assertSession()->pageTextContains('New submission added to CiviCRM Webform Test.');
+
+    $contact = $this->utils->wf_civicrm_api('Contact', 'get', [
+      'sequential' => 1,
+      'return' => ["tag"],
+      'first_name' => 'Frederick',
+      'last_name' => 'Pabst',
+    ])['values'][0];
+    $contactTags = explode(',', $contact['tags']);
+    $this->assertTrue(in_array('Child Tag A', $contactTags));
+    $this->assertTrue(in_array('Volunteer', $contactTags));
+  }
+
 }
