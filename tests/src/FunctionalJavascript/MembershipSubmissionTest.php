aab2623478b6477c49572622cd765c1c6fbf8ff2
KarinG <karin@semper-it.com>
2023-03-02 10:43:18 -0700

    Change to 365 days - 2024 is a leap year.

--- tests/src/FunctionalJavascript/MembershipSubmissionTest.php
+++ tests/src/FunctionalJavascript/MembershipSubmissionTest.php
@@ -183,7 +183,7 @@ final class MembershipSubmissionTest extends WebformCivicrmTestBase {
     $this->assertEquals($today,  $membership['join_date']);
     $this->assertEquals($today,  $membership['start_date']);
 
-    $this->assertEquals(date('Y-m-d', strtotime($today. ' +1 year')),  $membership['end_date']);
+    $this->assertEquals(date('Y-m-d', strtotime($today. ' +365 days')),  $membership['end_date']);
   }
 
   /**
