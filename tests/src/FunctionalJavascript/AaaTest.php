0dfcb1394f02d42b0da0d3d5dec41e0267f39e86
demeritcowboy <demeritcowboy@hotmail.com>
2025-11-03 13:29:13 -0500

    is it theme-related?

--- tests/src/FunctionalJavascript/AaaTest.php
+++ tests/src/FunctionalJavascript/AaaTest.php
@@ -15,7 +15,7 @@ use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 #[RunTestsInSeparateProcesses]
 class AaaTest extends WebDriverTestBase {
 
-  protected $defaultTheme = 'starterkit_theme';
+  protected $defaultTheme = 'claro';
 
   /**
    * Tests the thing
