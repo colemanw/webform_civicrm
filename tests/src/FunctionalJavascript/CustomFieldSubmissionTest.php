f268ae31bad5f4773f6d4313e6c3fba95a86eade
KarinG <karin@semper-it.com>
2025-11-14 18:56:21 -0700

    Update CustomFieldSubmissionTest.php
    Active checkbox is now: Enabled toggle. Check if this works.

--- tests/src/FunctionalJavascript/CustomFieldSubmissionTest.php
+++ tests/src/FunctionalJavascript/CustomFieldSubmissionTest.php
@@ -334,7 +334,7 @@ final class CustomFieldSubmissionTest extends WebformCivicrmTestBase {
       'query' => ['reset' => 1, 'action' => 'update', 'gid' => 1, 'id' => $this->_customFields['color_checkboxes']]
     ])->toString();
     $this->drupalGet($fieldURL);
-    $this->getSession()->getPage()->uncheckField('Active');
+    $this->getSession()->getPage()->uncheckField('Enabled');
     // $this->createScreenshot($this->htmlOutputDirectory . '/custom_field.png');
     $this->pressButtonOverride('_qf_Field_done-bottom');
 
@@ -348,7 +348,7 @@ final class CustomFieldSubmissionTest extends WebformCivicrmTestBase {
 
     //Re-enable the field.
     $this->drupalGet($fieldURL);
-    $this->getSession()->getPage()->checkField('Active');
+    $this->getSession()->getPage()->checkField('Enabled');
     $this->pressButtonOverride('_qf_Field_done-bottom');
     $this->assertPageNoErrorMessages();
 
