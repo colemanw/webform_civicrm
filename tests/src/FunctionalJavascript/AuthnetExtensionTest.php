6981571a3580bac2ad7ec27d4517b5f33c2668a4
KarinG <karin@semper-it.com>
2025-12-02 12:22:40 -0700

    Update AuthnetExtensionTest.php

--- tests/src/FunctionalJavascript/AuthnetExtensionTest.php
+++ tests/src/FunctionalJavascript/AuthnetExtensionTest.php
@@ -137,10 +137,11 @@ final class AuthnetExtensionTest extends WebformCivicrmTestBase {
     $this->getSession()->getPage()->checkField('Contribution Amount');
     $this->getSession()->getPage()->selectFieldOption('Currency', 'USD');
     $this->getSession()->getPage()->selectFieldOption('Financial Type', 'Donation');
-    $this->getSession()->getPage()->selectFieldOption('Payment Processor Mode', 'Test Mode');
 
     $this->assertCount(3, $this->getOptions('Payment Processor'));
     $this->getSession()->getPage()->selectFieldOption('Payment Processor', $this->paymentProcessorID);
+    $this->getSession()->getPage()->selectFieldOption('Payment Processor Mode', 'Test Mode');
+
     $this->enableBillingSection();
 
     $this->getSession()->getPage()->selectFieldOption('lineitem_1_number_of_lineitem', 2);
