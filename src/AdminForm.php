c821bafdd37f6648aa5ac87d89a26229075eca3a
francescbassas <francesc@babu.cat>
2026-07-03 11:08:03 +0200

    Fix default value retrieval for membership status message

--- src/AdminForm.php
+++ src/AdminForm.php
@@ -1052,7 +1052,7 @@ class AdminForm implements AdminFormInterface {
       '#type' => 'checkbox',
       '#title' => t('Show existing membership status message'),
       '#description' => t('When a contact with an existing membership fills out the form, display a message showing their current membership status and expiry date.'),
-      '#default_value' => (bool) wf_crm_aval($this->data, 'membership_options:show_status_message', 1, TRUE),
+      '#default_value' => (bool) wf_crm_aval($this->settings, 'membership_options:show_status_message', 1, TRUE),
       '#parents' => ['membership_options', 'show_status_message'],
     ];
     for ($c = 1, $cMax = count($this->data['contact']); $c <= $cMax; ++$c) {
