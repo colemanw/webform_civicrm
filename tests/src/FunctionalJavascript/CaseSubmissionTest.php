f726dd5c85f9b34f09f2672c3705f0a59a268b4d
KarinG <karin@semper-it.com>
2026-09-15 13:12:04 -0600

    Slow day...

--- tests/src/FunctionalJavascript/CaseSubmissionTest.php
+++ tests/src/FunctionalJavascript/CaseSubmissionTest.php
@@ -110,7 +110,7 @@ final class CaseSubmissionTest extends WebformCivicrmTestBase {
 
     // Make sure  $start_date is really only start_date (and not time)
     $start_date = $case_result['values'][0]['start_date'];
-    $start_date_only = date('Y-m-d', $start_date);
+    $start_date_only = date('Y-m-d', strtotime($start_date));
     
     $this->assertEquals($caseSubject, $case_result['values'][0]['subject']);
     $this->assertEquals(date('Y-m-d'), $start_date_only);
