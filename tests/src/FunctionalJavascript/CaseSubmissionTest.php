d0f68c08cca6dee090ab22879295e13f3a691e95
KarinG <karin@semper-it.com>
2026-09-15 16:54:59 -0600

    start_date now also appears to contain time (#1135)

--- tests/src/FunctionalJavascript/CaseSubmissionTest.php
+++ tests/src/FunctionalJavascript/CaseSubmissionTest.php
@@ -107,8 +107,13 @@ final class CaseSubmissionTest extends WebformCivicrmTestBase {
       'contact_id' => $this->_caseContact['id'],
     ]);
     $this->assertEquals(1, $case_result['count']);
+
+    // Make sure  $start_date is really only start_date (and not time)
+    $start_date = $case_result['values'][0]['start_date'];
+    $start_date_only = date('Y-m-d', strtotime($start_date));
+    
     $this->assertEquals($caseSubject, $case_result['values'][0]['subject']);
-    $this->assertEquals(date('Y-m-d'), $case_result['values'][0]['start_date']);
+    $this->assertEquals(date('Y-m-d'), $start_date_only);
   }
 
 }
