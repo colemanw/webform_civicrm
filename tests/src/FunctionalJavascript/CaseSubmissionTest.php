316f35582f18f9bb84dc2095db67f7a416d07463
KarinG <karin@semper-it.com>
2026-09-15 08:16:13 -0600

    Try this

--- tests/src/FunctionalJavascript/CaseSubmissionTest.php
+++ tests/src/FunctionalJavascript/CaseSubmissionTest.php
@@ -110,8 +110,7 @@ final class CaseSubmissionTest extends WebformCivicrmTestBase {
 
     // Make sure  $start_date is really only start_date (and not time)
     $start_date = $case_result['values'][0]['start_date'];
-    $dateObj = new DateTime($start_date);
-    $start_date_only = $dateObj->format('Y-m-d');
+    $start_date_only = date('Y-m-d', $start_date);
     
     $this->assertEquals($caseSubject, $case_result['values'][0]['subject']);
     $this->assertEquals(date('Y-m-d'), $start_date_only);
